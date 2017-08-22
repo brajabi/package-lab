72f84ac70af2c4b5ac6cfc2dbebd35a72ab1828e
Behnam <b.rajabifard@gmail.com>
2017-08-22 20:38:01 +0430

    Add: starter files & packages

---
+++ start.js
@@ -0,0 +1,19 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+// executes `pwd`
+child = exec("npm install", function (error, stdout, stderr) {
+
+  sys.print('stdout: ' + stdout);
+  sys.print('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+
+});
+
+// // or more concisely
+// var sys = require('sys')
+// var exec = require('child_process').exec;
+// function puts(error, stdout, stderr) { sys.puts(stdout) }
+// exec("ls -la", puts);
