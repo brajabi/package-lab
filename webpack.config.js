00c79ba9b72cdbab13665cc6bff2844f787ef17f
Behnam <b.rajabifard@gmail.com>
2017-08-23 12:25:38 +0430

    chore: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const webpack = require("webpack");
+const path = require('path');
 
 const config = {
-  entry: 'src/index.js',
+  entry: path.resolve('src/index.js'),
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  }
 };
 
 module.exports = config;
